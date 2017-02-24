271503ca34c48a62b92bafa54d011d3aaf7013ff
Dave Summers <dave@summdata.com>
2017-02-23 22:54:09 -0700

    add responseHistory and fix commandHistory scrolling

--- advent/public/js/controllers/AdventCtrl.js
+++ advent/public/js/controllers/AdventCtrl.js
@@ -1,16 +1,32 @@
 // public/js/controllers/AdventCtrl.js
-angular.module('AdventCtrl', []).controller('AdventController', function ($scope) {
+angular.module('AdventCtrl', []).controller('AdventController', function ($scope, $timeout, $GamePlayService) {
 
     var vm = $scope;
 
     $scope.tagline = 'A flash from the past!';
 
+    vm.loadGameMap();
+
     vm.responseHistory = "You are at the beginning";
+    vm.commandHistory = '';
     vm.cmd = 'Enter Command';
     
     vm.doCmd = function () {
-        debugger;
-        vm.responseHistory += "\n" + vm.cmd;
+       // debugger;
+        vm.commandHistory += "\n" + vm.cmd;
+        $timeout(function() {
+            var scroller = document.getElementById("commandHistory");
+            scroller.scrollTop = scroller.scrollHeight;
+            }, 0, false);
         vm.cmd = '';
+
+        //Get command results
+        vm.responseHistory = "We are in a tunnel, all twisted tunnels" + "\n" + vm.responseHistory;
     };
+
+    vm.loadGameMap= function(){
+        debugger;
+        var map = $gamePlayService(get());
+    };
+
 });
